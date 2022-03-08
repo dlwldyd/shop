fc6cfebff489f49f4b3d91bd37c7708d966bf84b
dlwldyd <dlwldyd9303@naver.com>
2022-03-08 22:29:58 +0900

    admin 추가

--- src/main/java/com/example/shop/init/InitAdmin.java
+++ src/main/java/com/example/shop/init/InitAdmin.java
@@ -4,6 +4,7 @@ import com.example.shop.domain.entity.Member;
 import com.example.shop.domain.enumtype.Role;
 import com.example.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,13 +14,15 @@ import javax.annotation.PostConstruct;
 public class InitAdmin {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostConstruct
     public void init() {
-        memberService.saveMember(new Member("admin",
+        memberService.saveMember(new Member(
+                "admin",
                 "관리자",
                 "aaaa@naver.com",
-                "admin",
+                passwordEncoder.encode("admin"),
                 "서울",
                 "01011111111",
                 Role.ADMIN));
